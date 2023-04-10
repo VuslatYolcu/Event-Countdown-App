8aab7446117712cd20ffb96f498ed576bd9916f0
VuslatYolcu <ylcvalt@gmail.com>
2023-04-10 22:16:36 +0300

    Add a navigation bar item

--- EventsApp/EventListViewController.swift
+++ EventsApp/EventListViewController.swift
@@ -17,8 +17,20 @@ class EventListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    private func setupViews() {
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let barButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(tappedRightBarButton))
+        barButtonItem.tintColor = .primary
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @objc private func tappedRightBarButton() {
+        print("tapped right bar button")
     }
 
 }
---
+++ EventsApp/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  EventsApp
+//
+//  Created by Vuslat Yolcu on 10.04.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
